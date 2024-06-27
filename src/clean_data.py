cf75c598f76b8e9f576403beb81361353b4be687
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-06-27 18:14:07 +0200

    clean artwork_data.csv and modify the cleaning of artist_data.csv

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
+
+# Function for convert data types 
+def convert_dtypes(df, desired_dtypes):
+    for column, dtype in desired_dtypes.items():
+        if column in df.columns:
+            if df[column].dtype != dtype:
+                # Converti la colonna al tipo desiderato
+                if dtype == int:
+                    df[column] = pd.to_numeric(df[column], errors='coerce').astype(int)
+                elif dtype == float:
+                    df[column] = pd.to_numeric(df[column], errors='coerce').astype(float)
+                elif dtype == str:
+                    df[column] = df[column].astype(str)
+
+
 artist_df = pd.read_csv('csv-files/artist_data.csv')
 artwork_df = pd.read_csv('csv-files/artwork_data.csv')
+
 
 # Clean artist_data.csv
 artist_df.drop_duplicates(inplace=True)     # Drop duplicates
@@ -9,7 +25,7 @@ artist_df.dropna(inplace=True)              # Drop rows with missing values
 
 # Verify the data types of each column and convert them to the correct data type
 # Desired data types
-desired_dtypes = {
+artist_desired_dtypes = {
     'id': int,
     'name': str,
     'gender': str,
@@ -20,20 +36,53 @@ desired_dtypes = {
     'url': str,
 }
 
-# Function for convert data types and handle null values
-def convert_dtypes(df, desired_dtypes):
-    for column, dtype in desired_dtypes.items():
-        if column in df.columns:
-            if df[column].dtype != dtype:
-                # Converti la colonna al tipo desiderato
-                if dtype == int:
-                    df[column] = pd.to_numeric(df[column], errors='coerce').fillna(0).astype(int)
-                elif dtype == str:
-                    df[column] = df[column].fillna('').astype(str)
-
 # Convert artist_df data types
-artist_df = convert_dtypes(artist_df, desired_dtypes)
+artist_df = convert_dtypes(artist_df, artist_desired_dtypes)
 
+# Handle null values
+artist_df['id'] = artist_df['id'].fillna(0)
+artist_df['name'] = artist_df['name'].fillna('')
+artist_df['gender'] = artist_df['gender'].fillna('')
+artist_df['yearOfBirth'] = artist_df['yearOfBirth'].fillna('')
+artist_df['url'] = artist_df['url'].fillna('')
+
+
+# Clean artwork_data.csv
+artwork_df.drop_duplicates(inplace=True)    # Drop duplicates
+artwork_df.dropna(inplace=True)             # Drop rows with missing values
+
+# Verify the data types of each column and convert them to the correct data type
+# Desired data types
+artwork_desired_types = {
+    'id': int,
+    'accession_number': str,
+    'artist': str,
+    'artistRole': str,
+    'artistId': int,
+    'title': str,
+    'dateText': str,
+    'medium': str,
+    'creditLine': str,
+    'year': int,
+    'acquisitionYear': int,
+    'types': str,
+    'width': int,
+    'height': int,
+    'depth': float,
+    'units': str,
+    'inscription': str,
+    'thumbnailCopyright': str,
+    'thumbnailUrl': str,
+    'url': str
+}
+
+# Convert artwork_df data types
+artwork_df = convert_dtypes(artwork_df, artwork_desired_types)
+
+# Handle null values
+artwork_df['id'] = artwork_df['id'].fillna(0)
+artwork_df['accession_number'] = artwork_df['accession_number'].fillna('')
+artwork_df['artistId'] = artwork_df['artistId'].fillna(0)
 
 
 # Save clen data
