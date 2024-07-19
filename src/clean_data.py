a8ece35e2d6045f0edb21ef8a6fcaa49715f5feb
nicolariccimaccarini <115147017+nicolariccimaccarini@users.noreply.github.com>
2024-07-19 14:33:13 +0200

    fixed data cleaning - add check firts key  integrity

--- src/clean_data.py
+++ src/clean_data.py
@@ -53,13 +53,17 @@ def clean_artist_data(df):
 def clean_artwork_data(df):
     # Drop the 'thumbnailCopyright' column
     df.drop(columns=['thumbnailCopyright'], inplace=True)
+
+    # Check first key integrity
+    df = check_fk_integrity(df)
     
     # Extract acquisitionYear and width from creditLine and dimension columns
-    df['acquisitionYear'] = df['creditLine'].str.extract(r'(\d{4})').iloc[:, 0]
-    df['width'] = df['dimensions'].str.extract(r'(\d+)\s*[xX]\s*\d+').astype(float).iloc[:, 0]
-    df['height'] = df['dimensions'].str.extract(r'\d+\s*[xX]\s*(\d+)').astype(float).iloc[:, 0]
+    df.loc[:, 'acquisitionYear'] = df['creditLine'].str.extract(r'(\d{4})').iloc[:, 0]
+    df.loc[:, 'width'] = df['dimensions'].str.extract(r'(\d+)\s*[xX]\s*\d+').astype(float).iloc[:, 0]
+    df.loc[:, 'height'] = df['dimensions'].str.extract(r'\d+\s*[xX]\s*(\d+)').astype(float).iloc[:, 0]
     
     # Extract type without dimensions from 'dimensions' column
+    df['dimensions'] = df['dimensions'].astype(str)
     df['types'] = df['dimensions'].apply(extract_type_without_dimensions)
     
     # Fill missing values
@@ -72,11 +76,12 @@ def clean_artwork_data(df):
     df['medium'] = df['medium'].fillna('Unknown')
     df['acquisitionYear'] = df['acquisitionYear'].fillna(0)
     df['thumbnailUrl'] = df['thumbnailUrl'].fillna('Unknown')
-    df['year'] = pd.to_numeric(df['year'].replace('no date', np.nan), errors='coerce').fillna(0)
-    df['acquisitionYear'] = pd.to_numeric(df['acquisitionYear'], errors='coerce').fillna(0)
-    df['width'] = pd.to_numeric(df['width'], errors='coerce').fillna(0)
-    df['height'] = pd.to_numeric(df['height'], errors='coerce').fillna(0)
-    df['depth'] = df['depth']
+    df['year'] = pd.to_numeric(df['year'].replace('no date', np.nan), errors='coerce').fillna(0).astype(int)
+    df['acquisitionYear'] = pd.to_numeric(df['acquisitionYear'], errors='coerce').fillna(0).astype(int)
+    df['width'] = pd.to_numeric(df['width'], errors='coerce').fillna(0).astype(int)
+    df['height'] = pd.to_numeric(df['height'], errors='coerce').fillna(0).astype(int)
+    df['depth'] = df['depth'].astype(int)
+    df['types'] = df['types'].fillna('Unknown')
 
     # Replace tumbnailUrl
     df['thumbnailUrl'] = df['thumbnailUrl'].str.replace("/www.", "/media.")
@@ -88,9 +93,17 @@ def clean_artwork_data(df):
 
 
 def extract_type_without_dimensions(dimension_str):
+    if pd.isna(dimension_str) or not isinstance(dimension_str, str):
+        return None
     match = re.search(r'\d', dimension_str) if any(char.isdigit() for char in dimension_str) else None
     text_part = dimension_str[:match.start()].strip() if match else dimension_str.strip()
     return text_part.replace(':', '').strip() or None
+
+def check_fk_integrity(df):
+    source_path = "/mnt/c/Users/nicol/OneDrive/Documenti/GitHub/Database-Project/csv-files"
+    artist_data_csv = os.path.join(source_path, "artist_data.csv")
+    artist_df = pd.read_csv(artist_data_csv)
+    return df[df['artistId'].isin(artist_df['id'])]
 
 
 def main():
@@ -102,7 +115,7 @@ def main():
     cleaned_artworks_data_csv = os.path.join(source_path, "cleaned_artwork_data.csv")
 
     artist_df = pd.read_csv(artist_data_csv)
-    artwork_df = pd.read_csv(artworks_data_csv)
+    artwork_df = pd.read_csv(artworks_data_csv, low_memory=False)
 
     # clean and save artist data
     cleaned_artist_df = clean_artist_data(artist_df.copy())
